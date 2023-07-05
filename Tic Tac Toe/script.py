b487b847dfe571b08b8e58ecee6d6793e97f899c
newwohh <nevoznvx@gmail.com>
2023-07-05 14:58:44 +0530

    tic tac toe

---
+++ Tic Tac Toe/script.py
@@ -0,0 +1,17 @@
+row1 = [" ", " ", " "]
+row2 = [" ", " ", " "]
+row3 = [" ", " ", " "]
+
+
+def display(row1, row2, row3):
+    print(row1)
+    print(row2)
+    print(row3)
+
+
+result = input("Enter Value: ")
+
+result_to_num = int(result)
+
+
+display(row1, row2, row3)

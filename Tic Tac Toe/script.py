5ab71187f599caba7fc343ad350b7640ee26f5c1
newwohh <nevoznvx@gmail.com>
2023-07-05 16:36:04 +0530

    tic-tac-toe

--- Tic Tac Toe/script.py
+++ Tic Tac Toe/script.py
@@ -9,9 +9,32 @@ def display(row1, row2, row3):
     print(row3)
 
 
-result = input("Enter Value: ")
+# result = input("Enter Value: ")
+# result_to_num = int(result)
+# position = int(input("Choose position: "))
 
-result_to_num = int(result)
+
+def user_choice():
+    choice = 'wrong'
+    ideal_range = range(0, 10)
+    within_range = False
+
+    while choice.isdigit() == False or within_range == False:
+        choice = input("Please enter a number (0-10)")
+
+        if choice.isdigit() == False:
+            print("Enter a digit: ")
+
+        if choice.isdigit() == True:
+            if int(choice) in ideal_range:
+                within_range = True
+            else:
+                print("Sorry cannot acceptable")
+                within_range = False
+
+    return int(choice)
 
 
-display(row1, row2, row3)
+# display(row1, row2, row3)
+
+user_choice()
